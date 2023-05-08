0c2f8db99550589c6fbbffbfc17f6ec40a770d14
teddyxiong53 <1073167306@qq.com>
2023-05-08 15:26:13 +0800

    优化说明

--- common.sh
+++ common.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 usage() {
     echo "
-example:
+需要设置code_dir、xml_file、version这3个环境变量。
+举例:
 export code_dir=/home/hanliang.xiong/work/a113l2/code
 export xml_file=buildroot-openlinux-202305-a113l2-ba400-spk.xml
 export version=buildroot-openlinux-202305-a113l2-ba400-spk"

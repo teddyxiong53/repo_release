61baec1ddd98425194df0d32830b2e69075c0822
teddyxiong53 <1073167306@qq.com>
2023-05-08 15:05:06 +0800

    3个必要的环境变量都加上判断，增加健壮性。

--- 01_gen_name_path.sh
+++ 01_gen_name_path.sh
@@ -7,6 +7,14 @@ if [[ "$code_dir" == "" ]]; then
     echo "please set code_dir in ENV"
     exit 1
 fi
+if [[ "$xml_file" == "" ]]; then
+    echo "please set xml_file in ENV"
+    exit 1
+fi
+if [[ "$version" == "" ]]; then
+    echo "please set version in ENV"
+    exit 1
+fi
 
 python3 gen_remote_url.py $code_dir
 
